28b7eca48d55f1c0491b5ff747406719eff79897
Ahmed Mohamed <149010186+ahmedmohamed0011@users.noreply.github.com>
2024-05-20 18:59:49 +0300

    Update main.py

--- YouTubeDownloaderProject/main.py
+++ YouTubeDownloaderProject/main.py
@@ -1,8 +1,10 @@
 import os
+import threading
+import time
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox  
 from tkinter import filedialog
-import threading
 from pytube import YouTube
 from pytube import *
 from pydub import AudioSegment
@@ -29,37 +31,34 @@ def ytDownloadmp3():
     link = linkEntry.get()
     folder = savePathEntry.get()
     YouTube(link, on_complete_callback=finish).streams.filter(only_audio='True').first().download(folder)
-
-
-
-#def ytDownloadmp3():
-#    status.config(text="Status: Downloading....")
-#    link = linkEntry.get()
-#    folder = savePathEntry.get()
-#    yt = YouTube(link, on_complete_callback=finish)
-#    stream = yt.streams.filter(only_audio=True).first()
-#    download_path = stream.download(folder)
-#    convert_to_mp3(download_path, folder)
-
-#def convert_to_mp3(download_path, folder):
-#    try:
-#        base, ext = os.path.splitext(download_path)
-#        audio = AudioSegment.from_file(download_path)
-#        mp3_path = base + '.mp3'
-#        audio.export(mp3_path, format="mp3")
-#        os.remove(download_path)
-#        status.config(text="Status: Download Finished!", bg="green")
-#    except Exception as e:
-#        status.config(text=f"Status: Conversion Error - {str(e)}", bg="red")
+    
 
 def finish(stream=None, chunk=None, file_handle=None, remaining=None):
     status.config(text="Status: Download Finished.!", bg="green")
+    messagebox.showinfo("Thanks", "Thank You For Using My App❤️..!")
+    clear()
 
 def start_download_thread_mp4():
     threading.Thread(target=ytDownloadmp4).start()
 
 def start_download_thread_mp3():
     threading.Thread(target=ytDownloadmp3).start()
+
+def clear ():
+    link = linkEntry.delete(0, "end")
+    path = savePathEntry.delete(0, "end")
+    status.config(text="Status: Ready.!", bg="white")
+
+def clearButton ():
+    link = linkEntry.delete(0, "end")
+    path = savePathEntry.delete(0, "end")
+    status.config(text="Status: Ready.!", bg="white")
+    messagebox.showinfo("Clear Info", "Clear Is Finished..!")
+
+def on_closing():
+    # Show information window
+    if messagebox.askokcancel("Quit", "This Program Developed By : Ahmed Mohamed ! \n To Support Me... \n InstaPay User : ahmed.mohamed5050@instapay \n Do You Want Quit ?"):
+        root.destroy()
 
 # GUI Section 
 
@@ -94,13 +93,14 @@ downloadButton.place(relx=0.7, rely=0.79, anchor="center")
 downloadButton = ttk.Button(root, text="Download Mp3!", width=25, command=start_download_thread_mp3)
 downloadButton.place(relx=0.3, rely=0.79, anchor="center")
 
+# Clear Button 
+clearButton = ttk.Button(root, text="Clear", command=clearButton)
+clearButton.place(rely=0.85, relx=0.8)
+
 # Status bar
 status = Label(root, text="Status: Ready", font="Calibre 10 italic", fg="black", bg="white", anchor="w")
 status.place(rely=1, anchor="sw", relwidth=1)
 
-# CopyRights
-copyrights = Label(root, text="Devolved By : Ahmed Mohamed !", font="Calibre 12 italic" )
-copyrights.place(rely=0.925, anchor="sw", )
-
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 root.mainloop()
